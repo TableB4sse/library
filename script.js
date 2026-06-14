3b007fa679c3e6d627e0afcf48e5237b5cea3ee6
TableB4sse <randomguy8242@gmail.com>
2026-06-14 17:37:29 +0200

    Add book constructor function

---
+++ script.js
@@ -0,0 +1,29 @@
+
+function Book(title, author, pages, isRead, id){
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+  this.id = id;
+};
+
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author} ${this.pages} pages, ${this.isRead ? 'read':'not read yet'}`;
+}
+
+function addBookToLibrary(title, author, pages, read, id, library){
+  const book = new Book(title, author, pages, read, id);
+  library.push(book);
+};
+
+const myLibrary = [];
+// const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false, crypto.randomUUID());
+
+addBookToLibrary("The Hobbit", "J.R.R Tolkien", 295, false, crypto.randomUUID(), myLibrary);
+
+console.log("Library: ", myLibrary);
+
+
