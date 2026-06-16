46b21a7215c6ec39a105ccb50e2dc561854ad68d
TableB4sse <randomguy8242@gmail.com>
2026-06-16 15:09:10 +0200

    Add auto implementation of books in the library and changed all the colors

--- script.js
+++ script.js
@@ -19,9 +19,68 @@ function addBookToLibrary(title, author, pages, read, id, library){
   library.push(book);
 };
 
+function removeBook(){
+  console.log("test remove book");
+};
+
+function displayBook(library){
+  const libraryContainer = document.querySelector(".library-container");
+
+  library.forEach((book) => {
+    const divBook = document.createElement("div");
+    const readButton = document.createElement("button");
+    const title = document.createElement("h2");
+    const author = document.createElement("p");
+    const pages = document.createElement("p");
+    const removeButton = document.createElement("button");
+
+    divBook.classList.add("book");
+
+    readButton.textContent = "READ";
+    if (book.isRead) {
+      readButton.classList.add("not-read");
+      readButton.classList.add("read");
+    } else {
+      readButton.classList.add("not-read");
+    }
+    readButton.addEventListener("click", () => {
+      book.isRead = !book.isRead;
+      readButton.classList.toggle("read");
+    });
+
+    title.textContent = book.title;
+    title.classList.add("title");
+    author.textContent = "Author: " + book.author;
+    author.classList.add("author");
+    pages.textContent = "Pages: " + book.pages;
+    pages.classList.add("pages");
+
+    removeButton.textContent = "Remove";
+    removeButton.classList.add("remove-btn");
+    removeButton.addEventListener("click", () => {
+      removeBook();
+    });
+
+    divBook.append(readButton, title, author, pages, removeButton);
+
+    libraryContainer.append(divBook);
+
+  });
+};
+
 const myLibrary = [
-  new Book("The Hobbit", "J.R.R Tolkien", 295, false, crypto.randomUUID()),
+  new Book("The Hobbit", "J.R.R. Tolkien", 295, false, crypto.randomUUID()),
   new Book("A Little Life", "Hanya Yanagihara", 738, true, crypto.randomUUID()),
+  new Book("1984", "George Orwell", 328, true, crypto.randomUUID()),
+  new Book("Dune", "Frank Herbert", 688, false, crypto.randomUUID()),
+  new Book("The Name of the Wind", "Patrick Rothfuss", 662, true, crypto.randomUUID()),
+  new Book("The Catcher in the Rye", "J.D. Salinger", 277, false, crypto.randomUUID()),
+  new Book("The Lord of the Rings", "J.R.R. Tolkien", 1178, true, crypto.randomUUID()),
+  new Book("Crime and Punishment", "Fyodor Dostoevsky", 671, false, crypto.randomUUID()),
+  new Book("The Martian", "Andy Weir", 387, true, crypto.randomUUID()),
+  new Book("Project Hail Mary", "Andy Weir", 496, false, crypto.randomUUID()),
+  new Book("The Count of Monte Cristo", "Alexandre Dumas", 1276, true, crypto.randomUUID()),
+  new Book("The Three-Body Problem", "Liu Cixin", 416, false, crypto.randomUUID()),
 ];
 
-
+displayBook(myLibrary);
